3465d4b9ab0d42abafc44a94190110943dad3fad
Toby Bettridge <toby@heavypenguin.com>
2022-01-07 17:12:17 +0000

    First test. Awww.

--- tests/test_withinforth.py
+++ tests/test_withinforth.py
@@ -1,4 +1,13 @@
 from withinforth import withinforth
+import unittest   # The test framework
 
-def test_x():
-    assert withinforth.x() == 10.0
+class CreateMachineTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.machine = withinforth.Interpreter()
+
+    def test_empty_command(self):
+        self.assertEqual(self.machine.run(""), "ok")
+
+if __name__ == '__main__':
+    unittest.main()
--- withinforth/withinforth.py
+++ withinforth/withinforth.py
@@ -2,3 +2,7 @@ from math import radians, cos, sin, asin, sqrt
 
 def x() -> float:
     return 10.0
+
+class Interpreter:
+    def run(self, code: str) -> str:
+        return "ok"
